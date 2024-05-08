fcad26a37804b12c6b65e3461dfa30c329c3bb71
alex <alexasensioparami@gmail.com>
2024-05-08 16:22:20 +0200

    operaciones

--- tests/test_resta.py
+++ tests/test_resta.py
@@ -10,6 +10,6 @@ class RestaTetsClass:
  
     def test_resta(self):
         assert resta(4,5) == -1
-        assert resta(-1,3) == 2
-        assert resta(-7,5) == -2
-        assert resta(-7,2) == -5
+        assert resta(-1,3) == -4
+        assert resta(-7,5) == -12
+        assert resta(-7,2) == -9
