f362eac687c53c61cbf6681496c2cf9a72244463
alex <alexasensioparami@gmail.com>
2024-05-08 16:39:00 +0200

    operaciones 56

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -1,19 +1,12 @@
 # En principio el alumno solo recibiría la definición de la función en el archivo correspondiente
 def suma(x,y):
-
-
     return x + y
-def resta(x,y):
 
- 
+def resta(x,y):
     return x - y
 
 def multiplicacion(x,y):
-
-    
     return x * y
 
 def division(x,y):
-
-    
     return x / y
